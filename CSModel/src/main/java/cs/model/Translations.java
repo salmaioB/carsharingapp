6451b5af77e27208e432e459df910e15fd733346
huret.arnaud <huret.arnaud@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-21 21:32:04 +0000

    Project CSModel: # Clean up (Modify id fields, field name different between the class and the database, etc...)

--- CSModel/src/main/java/cs/model/Translations.java
+++ CSModel/src/main/java/cs/model/Translations.java
@@ -2,24 +2,31 @@ package cs.model;
 
 public class Translations
 {
-	private Integer id;
+	private Integer idTranslation;
 	private Integer idTranslationValue;
 	private String valueSummary;
-	public Integer getId() {
-		return id;
+	
+	
+	public Integer getIdTranslation() {
+		return idTranslation;
 	}
-	public void setId(Integer id) {
-		this.id = id;
+	
+	public void setId(Integer idTranslation) {
+		this.idTranslation = idTranslation;
 	}
+	
 	public Integer getIdTranslationValue() {
 		return idTranslationValue;
 	}
+	
 	public void setIdTranslationValue(Integer idTranslationValue) {
 		this.idTranslationValue = idTranslationValue;
 	}
+	
 	public String getValueSummary() {
 		return valueSummary;
 	}
+	
 	public void setValueSummary(String valueSummary) {
 		this.valueSummary = valueSummary;
 	}
