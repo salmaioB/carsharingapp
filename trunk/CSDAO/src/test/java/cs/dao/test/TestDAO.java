bf2c68f6929977dc1353a42cc22f5d12e495d72b
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-22 14:25:13 +0000

    Suppression test objet User

--- trunk/CSDAO/src/test/java/cs/dao/test/TestDAO.java
+++ trunk/CSDAO/src/test/java/cs/dao/test/TestDAO.java
@@ -1,46 +1,18 @@
 package cs.dao.test;
 
 import cs.dao.dao.CustomerAccountsDAO;
-import cs.dao.dao.UserDAO;
 import cs.model.CustomerAccounts;
-import cs.model.User;
 import junit.framework.TestCase;
 
 public class TestDAO extends TestCase
 {
-/*
-	public void testloadUserDAO() {
-		UserDAO usa = new UserDAO();
-		User use = usa.load(1);
-
-		assertNotNull(use);
-
-		System.out.println("Read use" + use.getName());
-	}
-
-	
-	public void testaddUserDAO() {
-		UserDAO usa = new UserDAO();
-        User pers = new User();
-        pers.setAdresse("adresse");
-        pers.setCp("cp");
-        pers.setLastname("lastname");
-        pers.setName("name");
-        pers.setVille("ville");
-		Boolean b = usa.save(pers);
-
-		assertTrue(b);
-
-		System.out.println("Enregistré");
-	}
-	*/
 	public void testaddCustomerAccountDAO()
 	{
 		CustomerAccountsDAO dao = new CustomerAccountsDAO();
 		CustomerAccounts ca = new CustomerAccounts();
 		ca.setAcceptAnimals(0);
 		ca.setAcceptSmoker(0);
-		ca.setLogin("login");
+		ca.setCustomerLogin("login");
 		
 		Boolean b = dao.save(ca);
 
