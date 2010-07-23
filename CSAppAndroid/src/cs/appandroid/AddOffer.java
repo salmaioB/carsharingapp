63add943d30b9196f3e8e6c167a996a35c652fe6
huret.arnaud <huret.arnaud@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-23 00:32:33 +0000

    Project CSAndroidApp: Update the home menu adapter and create a new activity (AddOffer)

---
+++ CSAppAndroid/src/cs/appandroid/AddOffer.java
@@ -0,0 +1,14 @@
+package cs.appandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddOffer extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.addoffer);
+	}
+}
---
+++ CSAppAndroid/src/cs/appandroid/MyCustomGridView.java
@@ -0,0 +1,53 @@
+package cs.appandroid;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class MyCustomGridView extends GridView implements OnItemClickListener{
+
+  private Listener mListener;
+
+
+
+  public MyCustomGridView(Context context, AttributeSet attrs) {
+
+    super(context, attrs);
+
+    setOnItemClickListener(this);
+
+  }
+
+
+
+  public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+    if (mListener != null) {
+
+      mListener.onClick(position);
+
+    }
+
+  }
+
+
+
+  public void setListener(Listener l){
+
+    mListener = l;
+
+  }
+
+
+
+  public interface Listener{
+
+    void onClick(int position); 
+
+  }   
+
+}
