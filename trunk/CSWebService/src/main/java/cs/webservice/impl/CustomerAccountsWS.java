46dd1751e880f66721b8e8af1144bad336a8ed61
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-23 10:47:26 +0000

    Mise à jour du Web Service avec appel à la base

--- trunk/CSWebService/src/main/java/cs/webservice/impl/CustomerAccountsWS.java
+++ trunk/CSWebService/src/main/java/cs/webservice/impl/CustomerAccountsWS.java
@@ -1,6 +1,15 @@
 package cs.webservice.impl;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
 import com.opensymphony.xwork2.ActionSupport;
+
+import cs.dao.dao.CustomerAccountsDAO;
 import cs.model.CustomerAccounts;
 
 public class CustomerAccountsWS  extends ActionSupport {
@@ -8,35 +17,67 @@ public class CustomerAccountsWS  extends ActionSupport {
 	 * the Object (example User use) component the BeerWS are transmit automaticaly by JSON  
 	 */
 	private CustomerAccounts use;
-	private CustomerAccounts use2;
-	/**
-	 * Getteur and Setteur
-	 * @return
-	 */
+	private Integer id;
+	
+
 	public CustomerAccounts getUse() {
 		return use;
-	}
-	public CustomerAccounts getUse2() {
-		return use2;
-	}
-	public void setUse2(CustomerAccounts use2) {
-		this.use2 = use2;
 	}
 	public void setUse(CustomerAccounts use) {
 		this.use = use;
 	}
-	public String fonc() {
-		  System.out.println("call execute");
-		  use = new CustomerAccounts();
-		  use.setCustomerLogin("customerlogin");
-	      return SUCCESS;
+	public Integer getId() {
+		return id;
 	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	//private String /*CustomerAccounts*/ use;
+	/**
+	 * Getteur and Setteur
+	 * @return
+	 */
+
 	public String execute() throws Exception {
 		System.out.println("call execute");
-		use = new CustomerAccounts();
-		use2 = new CustomerAccounts();
-		use.setCustomerLogin("login");
-		use2.setCustomerLogin("login");
+		
+		System.out.println("id : "+id);
+
+		CustomerAccountsDAO cad = new CustomerAccountsDAO();
+		use = cad.load(id);
+		
+		System.out.println(use.getCustomerLogin());
+		/*
+		CustomerAccounts user = new CustomerAccounts();
+		
+		user.setCustomerLogin("login");
+		user.setCustomerPassword("password");
+		user.setCustomerType(0);
+		
+		user.setDatetimeLastCarSharing(null);
+		user.setDatetimeLastConnection(null);
+		user.setDatetimeLastOfferCreated(null);
+		user.setDatetimeRegistration(null);
+		
+		user.setAcceptAnimals(0);
+		user.setAcceptRadio(0);
+		user.setAcceptSmoker(0);
+		user.setAcceptToDiscuss(0);
+		user.setAcceptToMakeADetour(0);
+		
+		oos = new ObjectOutputStream() {
+		};
+		oos.writeObject(user);
+		
+		ObjectInputStream ois = new ObjectInputStream(){ };
+		
+		CustomerAccounts user2 = (CustomerAccounts) ois.readObject();
+		System.out.println(oos.toString());
+		*/
+		System.out.println("Fin du call execute");
 		return SUCCESS;
   }
 }  
