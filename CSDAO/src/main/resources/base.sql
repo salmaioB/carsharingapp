566b1a65851b31b07c8c0652f4eadc9903f968e4
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-27 15:23:31 +0000

    Nouvelle base

--- CSDAO/src/main/resources/base.sql
+++ CSDAO/src/main/resources/base.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.2deb1ubuntu0.2
+-- version 2.11.8.1deb5+lenny4
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Ven 23 Juillet 2010 à 16:49
--- Version du serveur: 5.0.75
--- Version de PHP: 5.2.6-3ubuntu4.5
+-- Généré le : Mar 27 Juillet 2010 à 17:17
+-- Version du serveur: 5.0.51
+-- Version de PHP: 5.2.6-1+lenny8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -35,10 +35,10 @@ CREATE TABLE IF NOT EXISTS `customer_accounts` (
   `accept_smoker` tinyint(1) unsigned NOT NULL,
   `accept_to_discuss` tinyint(1) unsigned NOT NULL,
   `accept_to_make_a_detour` tinyint(1) unsigned NOT NULL,
-  `datetime_registration` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `datetime_last_connection` timestamp NULL default '0000-00-00 00:00:00',
-  `datetime_last_offer_created` timestamp NULL default '0000-00-00 00:00:00',
-  `datetime_last_car_sharing` timestamp NULL default '0000-00-00 00:00:00',
+  `datetime_registration` datetime default NULL,
+  `datetime_last_connection` datetime default NULL,
+  `datetime_last_offer_created` datetime default NULL,
+  `datetime_last_car_sharing` datetime default NULL,
   PRIMARY KEY  (`_id_customer_account`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS `offers` (
   `number_of_place_initial` tinyint(1) unsigned NOT NULL,
   `number_of_place_remaining` tinyint(1) unsigned NOT NULL,
   `price_per_passenger` float(4,2) unsigned NOT NULL,
-  `datetime_started` datetime NOT NULL,
-  `datetime_ended` datetime NOT NULL,
+  `datetime_started` datetime default NULL,
+  `datetime_ended` datetime default NULL,
   PRIMARY KEY  (`_id_offer`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `offers` (
 --
 
 INSERT INTO `offers` (`_id_offer`, `_id_offer_type`, `_id_driver`, `title`, `description`, `number_of_place_initial`, `number_of_place_remaining`, `price_per_passenger`, `datetime_started`, `datetime_ended`) VALUES
-(1, 1, 1, 'title', 'description', 1, 1, 1.00, '2010-07-23 16:48:08', '2010-07-23 16:48:12');
+(1, 0, 0, 'Face de moule', 'Teub', 0, 0, 0.00, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -83,13 +83,32 @@ INSERT INTO `offers` (`_id_offer`, `_id_offer_type`, `_id_driver`, `title`, `des
 --
 
 CREATE TABLE IF NOT EXISTS `offers_to_customer_accounts` (
-  `_id_offers_to_customer_account` int(11) unsigned NOT NULL,
+  `_id_offers_to_customer_account` int(11) unsigned NOT NULL auto_increment,
   `_id_offer` int(11) unsigned NOT NULL,
-  `_id_customer_account` int(11) unsigned NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `_id_customer_account` int(11) unsigned NOT NULL,
+  PRIMARY KEY  (`_id_offers_to_customer_account`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `offers_to_customer_accounts`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offers_to_routes`
+--
+
+CREATE TABLE IF NOT EXISTS `offers_to_routes` (
+  `_id_offers_to_route` int(11) unsigned NOT NULL auto_increment,
+  `_id_offer` int(11) unsigned NOT NULL,
+  `_id_route` int(11) unsigned NOT NULL,
+  PRIMARY KEY  (`_id_offers_to_route`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `offers_to_routes`
 --
 
 
@@ -100,12 +119,13 @@ CREATE TABLE IF NOT EXISTS `offers_to_customer_accounts` (
 --
 
 CREATE TABLE IF NOT EXISTS `opinions` (
-  `_id_opinion` int(11) unsigned NOT NULL,
+  `_id_opinion` int(11) unsigned NOT NULL auto_increment,
   `_id_customer_account` int(11) unsigned NOT NULL,
   `positive_or_negative` tinyint(1) NOT NULL,
   `comment` text NOT NULL,
-  `datetime_created` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `datetime_created` datetime default NULL,
+  PRIMARY KEY  (`_id_opinion`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `opinions`
@@ -119,20 +139,24 @@ CREATE TABLE IF NOT EXISTS `opinions` (
 --
 
 CREATE TABLE IF NOT EXISTS `routes` (
-  `_id_route` int(11) unsigned NOT NULL,
-  `_id_offer` int(11) unsigned NOT NULL,
+  `_id_route` int(11) unsigned NOT NULL auto_increment,
   `_id_passenger` int(11) unsigned NOT NULL,
   `starting_address` varchar(60) NOT NULL,
+  `starting_city` varchar(60) NOT NULL,
   `finishing_address` varchar(60) NOT NULL,
+  `finishing_city` varchar(60) NOT NULL,
   `price` float(4,2) unsigned NOT NULL,
   `conversion_rate_to_euro` float(4,2) unsigned NOT NULL,
-  `currency_code` varchar(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `currency_code` varchar(10) NOT NULL,
+  PRIMARY KEY  (`_id_route`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `routes`
 --
 
+INSERT INTO `routes` (`_id_route`, `_id_passenger`, `starting_address`, `starting_city`, `finishing_address`, `finishing_city`, `price`, `conversion_rate_to_euro`, `currency_code`) VALUES
+(1, 0, 'toto', 'orleans', 'tata', 'paris', 0.00, 0.00, '');
 
 -- --------------------------------------------------------
 
@@ -141,10 +165,11 @@ CREATE TABLE IF NOT EXISTS `routes` (
 --
 
 CREATE TABLE IF NOT EXISTS `routes_to_customer_accounts` (
-  `_id_routes_to_customer_acount` int(11) unsigned NOT NULL,
+  `_id_routes_to_customer_acount` int(11) unsigned NOT NULL auto_increment,
   `_id_route` int(11) unsigned NOT NULL,
-  `_id_customer_account` int(11) unsigned NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `_id_customer_account` int(11) unsigned NOT NULL,
+  PRIMARY KEY  (`_id_routes_to_customer_acount`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `routes_to_customer_accounts`
@@ -158,10 +183,11 @@ CREATE TABLE IF NOT EXISTS `routes_to_customer_accounts` (
 --
 
 CREATE TABLE IF NOT EXISTS `translations` (
-  `_id_translation` int(11) unsigned NOT NULL,
+  `_id_translation` int(11) unsigned NOT NULL auto_increment,
   `_id_translation_value` int(11) unsigned NOT NULL,
-  `value_summary` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `value_summary` text NOT NULL,
+  PRIMARY KEY  (`_id_translation`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `translations`
@@ -175,11 +201,12 @@ CREATE TABLE IF NOT EXISTS `translations` (
 --
 
 CREATE TABLE IF NOT EXISTS `translation_values` (
-  `_id_translation_value` int(11) unsigned NOT NULL,
+  `_id_translation_value` int(11) unsigned NOT NULL auto_increment,
   `translation_value` text NOT NULL,
   `language_code` char(2) NOT NULL,
-  `country_code` char(2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `country_code` char(2) NOT NULL,
+  PRIMARY KEY  (`_id_translation_value`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `translation_values`
@@ -193,9 +220,10 @@ CREATE TABLE IF NOT EXISTS `translation_values` (
 --
 
 CREATE TABLE IF NOT EXISTS `vehicles` (
-  `_id_vehicle` int(11) unsigned NOT NULL,
-  `model` varchar(60) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `_id_vehicle` int(11) unsigned NOT NULL auto_increment,
+  `model` varchar(60) NOT NULL,
+  PRIMARY KEY  (`_id_vehicle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `vehicles`
