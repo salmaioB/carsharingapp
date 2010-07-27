3cc558d518ec3bc8a4b1a46adc1c1f1247504c97
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-27 13:42:16 +0000

    Nouvelle class de donnée

---
+++ CSModel/src/main/java/cs/model/TranslationValue.java
@@ -0,0 +1,34 @@
+package cs.model;
+
+public class TranslationValue
+{
+	private Integer id;
+	private String translationValue;
+	private String languageCode;
+	private String countryCode;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTranslationValue() {
+		return translationValue;
+	}
+	public void setTranslationValue(String translationValue) {
+		this.translationValue = translationValue;
+	}
+	public String getLanguageCode() {
+		return languageCode;
+	}
+	public void setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
+	}
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+}
