66e18e0620353e33fbd3852d7241a6fe25363d2f
huret.arnaud <huret.arnaud@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-28 01:30:01 +0000

    Project CSEngine: Clean up table names

--- trunk/CSEngine/src/main/java/cs/engine/action/RoutesToCustomerAccountEngineAction.java
+++ trunk/CSEngine/src/main/java/cs/engine/action/RoutesToCustomerAccountEngineAction.java
@@ -3,17 +3,17 @@ package cs.engine.action;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
-import cs.dao.dao.RouteToCustomerAccountDAO;
+import cs.dao.dao.RoutesToCustomerAccountDAO;
 import cs.model.RoutesToCustomerAccount;
 
 public class RoutesToCustomerAccountEngineAction
 {
-	private RouteToCustomerAccountDAO caBean;
+	private RoutesToCustomerAccountDAO caBean;
 	
 	public RoutesToCustomerAccountEngineAction()
 	{
 		XmlBeanFactory beanFactory =  new XmlBeanFactory(new ClassPathResource("bean.xml"));
-		caBean = (RouteToCustomerAccountDAO) beanFactory.getBean("RouteToCustomerAccountDAO");
+		caBean = (RoutesToCustomerAccountDAO) beanFactory.getBean("RouteToCustomerAccountDAO");
 	}
 	
 	public RoutesToCustomerAccount load(Integer id)
