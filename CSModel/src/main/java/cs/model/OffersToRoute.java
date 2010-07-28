126e2b82af78f72de69777a83835014a48e16751
huret.arnaud <huret.arnaud@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-28 01:30:40 +0000

    Project CSModel: Clean up table names

---
+++ CSModel/src/main/java/cs/model/OffersToRoute.java
@@ -0,0 +1,32 @@
+package cs.model;
+
+public class OffersToRoute
+{
+	private Integer id;
+	private Integer idOffer;
+	private Integer idRoute;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getIdOffer() {
+		return idOffer;
+	}
+	
+	public void setIdOffer(Integer idOffer) {
+		this.idOffer = idOffer;
+	}
+	
+	public Integer getIdRoute() {
+		return idRoute;
+	}
+	
+	public void setIdRoute(Integer idRoute) {
+		this.idRoute = idRoute;
+	}
+}
