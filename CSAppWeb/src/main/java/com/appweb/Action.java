43534c2ec0d90ea70519316cf57ff5d7c29f0d1c
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-29 14:26:09 +0000

    Ajout gestion variable de session ( actuellement utilisation de la langue en variable de session) + Ajout Objet variable Parametre GET et POST

--- CSAppWeb/src/main/java/com/appweb/Action.java
+++ CSAppWeb/src/main/java/com/appweb/Action.java
@@ -1,17 +1,30 @@
 package com.appweb;
 
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-import cs.engine.action.CustomerAccountEngineAction;
-import cs.engine.action.OfferEngineAction;
 import cs.engine.translation.TranslationEngine;
-import cs.model.CustomerAccount;
-import cs.model.Offer;
 
 public class Action extends ActionSupport
 {
+	private static final long serialVersionUID = 2352769285278947648L;
 	private TranslationEngine tr;
+	private Map session;
+	private Map parameters;
+	public Map getParameters() {
+		return parameters;
+	}
+	public String langage;
 	
+	public String getLangage() {
+		if(langage == null ) langage ="";
+		return langage;
+	}
+	public void setLangage(String langage) {
+		this.langage = langage;
+	}
 	public TranslationEngine getTr() {
 		return tr;
 	}
@@ -21,6 +34,29 @@ public class Action extends ActionSupport
 
 	public Action()
 	{
+		super();
+		//Récupère les variables de sessions et paramètre
+		session = ActionContext.getContext().getSession();
+		parameters = ActionContext.getContext(). getParameters();
+
+		// Si paramètre existe alors set la nouvelle langue
+		if(parameters.get("langage") != null)
+			setLangage( ((String[])parameters.get("langage"))[0] );
+		
+		//Si pas de session déclaré ou si nouveau langugage définit
+		if( session.get("language") == null || !getLangage().equals(""))
+		{
+			if( getLangage().equals("") )
+				session.put("langage","fr");
+			else
+				session.put("langage",getLangage());
+		}
+		//Initialisation de l'objet de translation avec la langue usuel
 		tr = new TranslationEngine();
+		String country = (String) session.get("langage");
+		tr.setCountry(country);
+	}
+	public Map getSession() {
+		return session;
 	}
 }
