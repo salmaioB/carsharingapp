1c33eef8ed91831890e38a218d7bb8d239a53c51
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-29 12:18:32 +0000

    Déplacement du fichier AccueilAction + suppression des traitements commun dans le fichier AccueilAction

---
+++ trunk/CSAppWeb/src/main/java/com/appweb/action/AccueilAction.java
@@ -0,0 +1,74 @@
+package com.appweb.action;
+
+import com.appweb.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+import cs.engine.action.CustomerAccountEngineAction;
+import cs.engine.action.OfferEngineAction;
+import cs.engine.translation.TranslationEngine;
+import cs.model.CustomerAccount;
+import cs.model.Offer;
+
+public class AccueilAction extends Action
+{
+
+	private static final long serialVersionUID = 1L;
+	public static final String MESSAGE = "";
+	private String message;
+	private String type;
+	
+	public String getType() {
+		if(type==null)
+			type="";
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMessage()
+	{
+		if(message==null)
+			message="";
+		return message;
+	}
+	
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+
+	public String execute()
+	{
+		if(getType().equals("CustomerAccounts"))
+		{
+			CustomerAccountEngineAction u = new CustomerAccountEngineAction();
+		
+			CustomerAccount use = u.load(new Integer(1));
+			if(use!= null)
+			{
+				String chaine = use.getCustomerLogin() +"-" + use.getCustomerPassword() + use.getDatetimeRegistration() +"|"+ use.getDatetimeLastCarSharing() +"|"+ use.getDatetimeLastConnection() +"|"+ use.getDatetimeLastOfferCreated() ;
+				setMessage(chaine);
+			}
+			else
+			{
+				setMessage("use not found");
+			}
+		}
+		if(getType().equals("Offer"))
+		{
+			OfferEngineAction u = new OfferEngineAction();
+		
+			Offer  offer = u.load(new Integer(1));
+			if(offer!= null)
+			{
+				setMessage(offer.getDescription() );
+			}
+			else
+			{
+				setMessage("offer not found");
+			}
+		}
+		return SUCCESS;
+	}
+}
