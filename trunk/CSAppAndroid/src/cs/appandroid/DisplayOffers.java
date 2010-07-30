3afd4a1eb4ada7ef786f760e9e5cbb7ceb924fa3
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-07-30 15:21:12 +0000

    Suppression insertion du packadge DAO

--- trunk/CSAppAndroid/src/cs/appandroid/DisplayOffers.java
+++ trunk/CSAppAndroid/src/cs/appandroid/DisplayOffers.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import cs.dao.dao.OfferDAO;
 import cs.model.Offer;
 import cs.webservice.OffersWS;
 import android.app.Activity;
