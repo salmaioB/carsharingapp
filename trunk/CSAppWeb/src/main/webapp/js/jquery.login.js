cac40345612baa55a8fe75f1e0359838436e5241
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-08 13:43:04 +0000

    jquery ajax

--- trunk/CSAppWeb/src/main/webapp/js/jquery.login.js
+++ trunk/CSAppWeb/src/main/webapp/js/jquery.login.js
@@ -1,23 +1,107 @@
 $(document).ready(function()
 {
-	$('input.toto').click(function()
-	{
-		alert('Handler for .click() called.');
-		
-		var postData = $("input", '#login_form').serialize();
-		alert(postData);
-		
-		$.ajax({
-			    method: 'post',
-	    		url: 'http://localhost:8080/CSAppWeb/LoginActionAjax',
-	    		data: postData,
-	    		success: function(data)
-	    		{
-        	 	 $('#divToUpdate').html(data);
-	    		}
-	  	  	  });
-		
-		return false;
-	});
-});
-
+	//Ajout des évenements ajax
+        $('input.check').click(function()
+        {
+                var postData = $("input", '#login_form').serialize();
+                
+                $.ajax({
+                        method: 'post',
+                        url: 'http://localhost:8888/CSAppWeb/LoginActionAjax',
+                        data: postData,
+                        success: function(data)
+                        {
+                         $('#divToLogin').html(data);
+                         $('#containerLeft').html("");
+                        }
+                          });
+                
+                return false;
+        });
+        $('input.chekin').click(function()
+                {
+                        var postData = $("input", '#checkin_form').serialize();
+                        
+                        $.ajax({
+                                method: 'post',
+                                url: 'http://localhost:8888/CSAppWeb/CheckInAjax',
+                                data: postData,
+                                success: function(data)
+                                {
+                                 $('#divToLogin').html(data);
+                                }
+                                  });
+                        
+                        return false;
+                });
+        $('input.checkout').click(function()
+                {
+                        var postData = $("input", '#checkout_form').serialize();
+                        
+                        $.ajax({
+                                method: 'post',
+                                url: 'http://localhost:8888/CSAppWeb/CheckOutAjax',
+                                data: postData,
+                                success: function(data)
+                                {
+                                 $('#divToLogin').html(data);
+                                }
+                                  });
+                        
+                        return false;
+                });
+        $('input.checkAddress').click(function()
+                {
+                        var postData = $("input", '#checkaddress_form').serialize();
+                        
+                        $.ajax({
+                                method: 'post',
+                                url: 'http://localhost:8888/CSAppWeb/CheckAddress',
+                                data: postData,
+                                success: function(data)
+                                {
+                                 $('#profil').html(data);
+                                 
+                                 initialize();
+                                }
+                                  });
+                        
+                        return false;
+                });
+        $('input.Cpost').click(function()
+                {
+                        var postData = $("input", '#post_form').serialize();
+                        
+                        $.ajax({
+                                method: 'post',
+                                url: 'http://localhost:8888/CSAppWeb/PrintMapPost',
+                                data: postData,
+                                success: function(data)
+                                {
+                                 $('#post').html(data);
+                                 
+                                 initialize();
+                                }
+                                  });
+                        
+                        return false;
+                });
+        $('input.Csearch').click(function()
+                {
+                        var postData = $("input", '#search_form').serialize();
+                        
+                        $.ajax({
+                                method: 'post',
+                                url: 'http://localhost:8888/CSAppWeb/PrintMapSearch',
+                                data: postData,
+                                success: function(data)
+                                {
+	                                 $('#search').html(data);
+	                                 
+	                                 initializeMapParcours();
+                                }
+                                  });
+                        
+                        return false;
+                });
+})
