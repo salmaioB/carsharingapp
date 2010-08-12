24a8a803f888def027dac82368875b6186e4b1f2
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-12 17:55:14 +0000

    DAO message

---
+++ CSDAO/src/main/java/cs/dao/dao/MessageDAO.java
@@ -0,0 +1,97 @@
+package cs.dao.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import cs.dao.util.HibernateUtil;
+import cs.model.Message;
+import cs.model.Offer;
+import cs.dao.DAO;
+
+public class MessageDAO extends DAO
+{
+	/**
+	 * Constructor by default
+	 */
+	public MessageDAO()
+	{
+		System.out.println("[Spring] load class:cs.dao.dao.MessageDAO");
+	}
+	
+	public Message load(Integer id)
+	{
+		 //Open session
+		 Session session = HibernateUtil.currentSession();
+		 Transaction tx = session.beginTransaction();
+		 session.beginTransaction();
+
+		 //Load Data object
+		 Message of = (Message) session.load( Message.class, id );
+		 
+		 tx.commit();
+
+		 return of;
+	}
+	
+	public Boolean save(Message of)
+	{
+		 Session session = HibernateUtil.currentSession();		   
+		 Transaction tx = session.beginTransaction();
+  
+		 session.beginTransaction();
+
+		 session.save(of);
+
+		 tx.commit();
+		 HibernateUtil.closeSession();
+
+		 return true;
+	}
+	/**
+	 * Function count the number of message for a customer
+	 */
+	public Integer nbMessageNotRead(Integer idCustomerAccount)
+	{
+		Integer count = 0;
+	     Session session = HibernateUtil.currentSession();
+	     String sqlQuery = "SELECT * " +
+		    "FROM message m " +
+		    "WHERE m._id_customer_account = " + idCustomerAccount +
+	        " AND m.read = 0 ";
+	     
+	     System.out.println(sqlQuery);
+	     
+	     Query query = session.createSQLQuery(sqlQuery).addEntity(Message.class);
+	     count = query.list().size();
+	     
+		HibernateUtil.closeSession();
+		
+		return count;
+	}
+	/**
+	 * Function to search message
+	 * by id idCustomerAccount
+	 */
+	public List<Message> loadSearchMessagesNotRead(Integer idCustomerAccount)
+	{
+     Session session = HibernateUtil.currentSession();
+     
+     String SQLQuery = "SELECT * " +
+					   "FROM message " +
+				       "WHERE _id_customer_account = " + idCustomerAccount +
+				       " AND read = 0 ";
+     
+     
+     System.out.println(SQLQuery);
+     
+     Query query = session.createSQLQuery(SQLQuery).addEntity(Message.class);
+     List<Message> offers = query.list();
+	 
+     HibernateUtil.closeSession();
+     
+	 return offers;
+	}
+}
