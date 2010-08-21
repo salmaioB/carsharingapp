8956410d1bfba3325d2583c47bd50053f2261e8a
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-21 16:26:53 +0000

    Amélioration de la gestion des messages

---
+++ CSAppWeb/src/main/java/com/appweb/action/ajax/MessageSend.java
@@ -0,0 +1,45 @@
+package com.appweb.action.ajax;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.appweb.Action;
+
+import cs.dao.dao.CustomerAccountDAO;
+import cs.dao.dao.MessageDAO;
+import cs.engine.action.CustomerAccountEngineAction;
+import cs.engine.action.MessageWithCustomerAccountAction;
+import cs.model.CustomerAccount;
+import cs.model.Message;
+import cs.model.MessageWithCustomerAccount;
+
+public class MessageSend extends Action
+{
+    private List<MessageWithCustomerAccount> listMessageSend;
+
+
+	public MessageSend()
+    {
+    	System.out.println("Construct MessageSend");
+    }
+
+	public String execute() throws Exception
+	{
+		System.out.println("execute MessageSend");
+		MessageWithCustomerAccountAction messageEngine  = new MessageWithCustomerAccountAction();
+		
+		listMessageSend = new ArrayList<MessageWithCustomerAccount>();
+		listMessageSend.addAll( messageEngine.loadMessageWithCustomerAccountSend( getCustomerAccount().getId() ) );
+		
+		System.out.println("Nombre de message send : " + listMessageSend.size() );
+		return SUCCESS;
+	}
+
+	public List<MessageWithCustomerAccount> getListMessageSend() {
+		return listMessageSend;
+	}
+
+	public void setListMessageSend(List<MessageWithCustomerAccount> listMessageSend) {
+		this.listMessageSend = listMessageSend;
+	}
+}
