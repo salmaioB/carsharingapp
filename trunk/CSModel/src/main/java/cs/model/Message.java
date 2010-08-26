e91d09aa4254320a0d2ed4b55571b769003038a8
huret.arnaud <huret.arnaud@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-26 15:38:36 +0000

    CSDAO and CSModel: fix message mapping

--- trunk/CSModel/src/main/java/cs/model/Message.java
+++ trunk/CSModel/src/main/java/cs/model/Message.java
@@ -4,7 +4,8 @@ import java.util.Date;
 
 public class Message
 {
-	private Integer id; 
+	private Integer id;
+	private Integer idOffer;
 	private String title;
 	private String content;
 	private Integer idCustomerAccount;
@@ -12,53 +13,73 @@ public class Message
 	private Date dateTimeWrited;
 	private Integer idCustomerTransmitter;
 	
-	public Integer getIdCustomerTransmitter() {
-		return idCustomerTransmitter;
-	}
-	public void setIdCustomerTransmitter(Integer idCustomerTransmitter) {
-		this.idCustomerTransmitter = idCustomerTransmitter;
-	}
-
-
+	
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+	public Integer getIdOffer() {
+		return idOffer;
+	}
+
+	public void setIdOffer(Integer idOffer) {
+		this.idOffer = idOffer;
+	}
+
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getContent() {
 		return content;
 	}
+	
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
 	public Integer getIdCustomerAccount() {
 		return idCustomerAccount;
 	}
+	
 	public void setIdCustomerAccount(Integer idCustomerAccount) {
 		this.idCustomerAccount = idCustomerAccount;
+	}
+	
+	public Integer getIsRead() {
+		return isRead;
+	}
+	
+	public void setIsRead(Integer isRead) {
+		this.isRead = isRead;
+	}
+	
+	public Date getDateTimeWrited() {
+		return dateTimeWrited;
+	}
+	
+	public void setDateTimeWrited(Date dateTimeWrited) {
+		this.dateTimeWrited = dateTimeWrited;
+	}
+	
+	public Integer getIdCustomerTransmitter() {
+		return idCustomerTransmitter;
+	}
+	
+	public void setIdCustomerTransmitter(Integer idCustomerTransmitter) {
+		this.idCustomerTransmitter = idCustomerTransmitter;
 	}
 	
 	public String toString()
 	{
 		return getId() + '/' + getTitle() + '/' + getContent() + '/' + getIdCustomerAccount() + ' ' + getIdCustomerTransmitter() +' ' + getDateTimeWrited() + ' ' +getIsRead();
-	}
-	public Integer getIsRead() {
-		return isRead;
-	}
-	public void setIsRead(Integer isRead) {
-		this.isRead = isRead;
-	}
-	public Date getDateTimeWrited() {
-		return dateTimeWrited;
-	}
-	public void setDateTimeWrited(Date dateTimeWrited) {
-		this.dateTimeWrited = dateTimeWrited;
 	}
 }
