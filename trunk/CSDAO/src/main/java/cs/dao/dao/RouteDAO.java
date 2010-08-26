7347f2b6860491533536e7334eb4304f4576636c
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-26 11:51:03 +0000

    Fonction chargeant une liste de route d'une offre

--- trunk/CSDAO/src/main/java/cs/dao/dao/RouteDAO.java
+++ trunk/CSDAO/src/main/java/cs/dao/dao/RouteDAO.java
@@ -1,9 +1,14 @@
 package cs.dao.dao;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import cs.dao.util.HibernateUtil;
+import cs.model.Offer;
 import cs.model.Route;
 
 import cs.dao.DAO;
@@ -17,7 +22,27 @@ public class RouteDAO extends DAO
 	{
 		System.out.println("[Spring] load class:cs.dao.dao.RouteDAO");
 	}
-	
+
+	public List<Route> loadRoutes(Integer idOffer)
+	{
+		Session session = HibernateUtil.currentSession();
+		
+		String SQLQuery = "SELECT * " +
+						  " FROM routes, offers_to_routes " +
+		   		          " WHERE offers_to_routes._id_offer =  " + idOffer.toString() +
+		   		          " AND routes._id_route = offers_to_routes._id_route " +
+		   		          " order by route_order asc" ;
+
+		System.out.println(SQLQuery);
+
+		Query query = session.createSQLQuery(SQLQuery).addEntity(Route.class);
+		
+		List<Route> routes = query.list();	
+		
+		HibernateUtil.closeSession();
+		
+		return routes;
+	}
 	public Route load(Integer id)
 	{
 		 //Open session
