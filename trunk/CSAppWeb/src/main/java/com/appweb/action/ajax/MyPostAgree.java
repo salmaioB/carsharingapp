c9662ca09af711849c84cc550a4a8f8079005336
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-26 12:48:54 +0000

    Nouvelle Action mes offres

---
+++ trunk/CSAppWeb/src/main/java/com/appweb/action/ajax/MyPostAgree.java
@@ -0,0 +1,47 @@
+package com.appweb.action.ajax;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.appweb.Action;
+
+import cs.dao.dao.CustomerAccountDAO;
+import cs.dao.dao.MessageDAO;
+import cs.dao.dao.OfferDAO;
+import cs.dao.spring.SpringDAO;
+import cs.engine.action.CustomerAccountEngineAction;
+import cs.engine.action.MessageWithCustomerAccountAction;
+import cs.engine.spring.SpringEngine;
+import cs.model.CustomerAccount;
+import cs.model.Message;
+import cs.model.MessageWithCustomerAccount;
+import cs.model.OfferWithCustomerAccount;
+
+public class MyPostAgree extends Action
+{
+   List<OfferWithCustomerAccount> owca;
+   
+	public List<OfferWithCustomerAccount> getOwca() {
+		return owca;
+	}
+
+public void setOwca(List<OfferWithCustomerAccount> owca) {
+	this.owca = owca;
+}
+
+	public MyPostAgree()
+    {
+    	System.out.println("Construct MyPostAgree");
+    }
+	public String execute() throws Exception
+	{
+		System.out.println("execute MyPostAgree");
+		OfferDAO offerDAO = SpringDAO.getSpring().getOfferDAO();
+		
+		owca = offerDAO.loadSearchOffers(null, null, getCustomerAccount().getId() );
+		System.out.println("owca.get(0).getAcceptAnimals() : " + owca.get(0).getCustomerAccount().getAcceptAnimals());
+		System.out.println("owca.get(0).getGender() : " + owca.get(0).getCustomerAccount().getGender());
+		return SUCCESS;
+	}
+}
