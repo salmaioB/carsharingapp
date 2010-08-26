0a153bfccd1c3df6c2c961a81dafd59fcd07af52
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-26 11:49:26 +0000

    Amélioration du service

--- CSAppWeb/src/main/java/com/appweb/action/ajax/MessageWriteResponse.java
+++ CSAppWeb/src/main/java/com/appweb/action/ajax/MessageWriteResponse.java
@@ -1,16 +1,42 @@
 package com.appweb.action.ajax;
 
+import java.util.Date;
+
 import com.appweb.Action;
 
+import cs.dao.dao.MessageDAO;
+import cs.dao.spring.SpringDAO;
 import cs.engine.action.MessageWithCustomerAccountAction;
 import cs.engine.spring.SpringEngine;
+import cs.model.Message;
 
 public class MessageWriteResponse extends Action
 {
     private Integer idMessage;
 	private String content;
+	private Integer idCustomer;
+	private String contentSendSearch;
+	private String titleSendSearch;
 	
-    public Integer getIdMessage() {
+	public String getContentSendSearch() {
+		return contentSendSearch;
+	}
+	public void setContentSendSearch(String contentSendSearch) {
+		this.contentSendSearch = contentSendSearch;
+	}
+	public String getTitleSendSearch() {
+		return titleSendSearch;
+	}
+	public void setTitleSendSearch(String titleSendSearch) {
+		this.titleSendSearch = titleSendSearch;
+	}
+	public Integer getIdCustomer() {
+		return idCustomer;
+	}
+	public void setIdCustomer(Integer idCustomer) {
+		this.idCustomer = idCustomer;
+	}
+	public Integer getIdMessage() {
 		return idMessage;
 	}
 	public void setIdMessage(String idMessage) {
@@ -35,15 +61,35 @@ public class MessageWriteResponse extends Action
 	public String execute() throws Exception
 	{
 		System.out.println("execute MessageWrite");
-		System.out.println("idMessage : " + getIdMessage() );
-		System.out.println("content : " + getContent() );
-		MessageWithCustomerAccountAction messageEngine  = SpringEngine.getSpring().getMessageWithCustomerAccountAction();//new MessageWithCustomerAccountAction();
-		StringBuilder title = new StringBuilder();
-		title.append("RE : ");
-		title.append( messageEngine.load(idMessage).getMessage().getTitle() );
-
-		messageEngine.sendMessageResponse(title.toString(), getContent(), getIdMessage());
-
+		MessageWithCustomerAccountAction messageEngine  = SpringEngine.getSpring().getMessageWithCustomerAccountAction();
+		MessageDAO messageDAO = SpringDAO.getSpring().getMessageDAO();
+		if(idMessage!=null)
+		{	
+			System.out.println("idMessage : " + getIdMessage() );
+			System.out.println("content : " + getContent() );
+			//new MessageWithCustomerAccountAction();
+			StringBuilder title = new StringBuilder();
+			title.append("RE : ");
+			title.append( messageEngine.load(idMessage).getMessage().getTitle() );
+	
+			messageEngine.sendMessageResponse(title.toString(), getContent(), getIdMessage());
+		}
+		else
+		{
+			System.out.println("write new message");
+			System.out.println("idCustomer : " + idCustomer);
+			System.out.println("contentSendSearch : " + contentSendSearch);
+			System.out.println("titleSendSearch : " + titleSendSearch);
+			Message message = new Message();
+			message.setIdCustomerAccount(idCustomer);
+			message.setIdCustomerTransmitter(getCustomerAccount().getId());
+			message.setContent(contentSendSearch);
+			message.setDateTimeWrited(new Date());
+			message.setIsRead(0);
+			message.setTitle(titleSendSearch);
+			
+			messageDAO.save(message);
+		}
 		return SUCCESS;
 	}
 }
