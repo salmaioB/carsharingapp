4db901ea1f35268b8921a6c5726f8af947bb8049
olivier.olife <olivier.olife@05b1d5a8-22e5-11df-8491-c72ddd2877f1>
2010-08-29 10:03:35 +0000

    Nouvelle action pour revenir a la création d'une offre apres l enregistrement

---
+++ CSAppWeb/src/main/java/com/appweb/action/ajax/PostEtape1.java
@@ -0,0 +1,13 @@
+package com.appweb.action.ajax;
+
+import com.appweb.Action;
+
+public class PostEtape1 extends Action
+{
+	
+	public String execute() throws Exception
+	{
+		System.out.println("execute PostEtape1");
+		return SUCCESS;
+	}
+}
